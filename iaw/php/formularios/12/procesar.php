f95aebfeaa289669a7ab99a9df67531ca7eece63
Athos Orio <athos.oc@gmail.com>
2017-12-18 18:04:08 +0100

    php

---
+++ iaw/php/formularios/12/procesar.php
@@ -0,0 +1,7 @@
+<?php
+  $tamano = $_REQUEST['tamano'];
+?>
+ <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width='<?php echo $tamano;?>px' height='<?php echo $tamano;?>px' >
+  <rect height='<?php echo $tamano;?>px' width='<?php echo $tamano;?>px' fill='white' stroke="black"
+   stroke-width="4" ></rect>
+</svg>
