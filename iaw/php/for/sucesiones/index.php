7316893e69bc9e598f1829fd6a90f489c0dcc2b4
Athos Orio <athos.oc@gmail.com>
2018-01-19 18:52:34 +0100

    sucesiones

---
+++ iaw/php/for/sucesiones/index.php
@@ -0,0 +1,38 @@
+<?php
+// $i=1
+
+for($i=1;$i<9;$i++){
+  echo $i+1 . " ";
+}
+
+echo "<br />";
+
+for($i=1;$i<16;$i=$i+2){
+  echo $i+1 . " ";
+}
+
+echo "<br />";
+
+for($i=1;$i<25;$i=$i+3){
+  echo $i+4 . " ";
+}
+
+echo "<br />";
+
+for($i=1;$i<30;$i=$i+5){
+  echo $i-1 . " ";
+}
+
+echo "<br />";
+
+for($i=1;$i<8;$i++){
+  echo 45-$i*5 . " ";
+}
+
+
+echo "<br />";
+
+for($i=1;$i>-43;$i=$i-6){
+  echo $i-8 . " ";
+}
+?>
